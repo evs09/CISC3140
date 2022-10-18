372ee58e7485a14569e88de5d38e90bae62e62e8
Evelyn Velez <67163723+evs09@users.noreply.github.com>
2022-10-18 17:44:17 -0400

    fix second custom format calculation, allow user to re-enter dates if invalid inputs

--- lab1/DueDateCalculator.java
+++ lab1/DueDateCalculator.java
@@ -5,8 +5,11 @@ import java.time.format.DateTimeFormatter;
 
 public class DueDateCalculator {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
+        getUserInput();
         
+    }
+    
+    public static void getUserInput(){
         LocalDate today = java.time.LocalDate.now();
         LocalDate l1 = LocalDate.of(2022, 9, 28);
         LocalDate l2 = LocalDate.of(2022,10,26);
@@ -17,6 +20,7 @@ public class DueDateCalculator {
         String[] dateTitles = new String[] {"Today: ", "Lab 1: ", "Lab 2: ", "Lab 3: ", "Lab 4: "};
         LocalDate[] dueDates = new LocalDate[] {today, l1, l2, l3, l4};
         
+        Scanner input = new Scanner(System.in);
         System.out.println("Please select two from the following, or enter two dates (MM/DD/YYYY or MM-DD): ");
         
         for(int i = 0; i < dueDates.length; i++){
@@ -25,7 +29,6 @@ public class DueDateCalculator {
         
         String a = input.next();
         String b = input.next();
-        
         
         if(a.length() <= 2){
             int d1 = Integer.parseInt(a);
@@ -36,7 +39,6 @@ public class DueDateCalculator {
         else{
             getDaysLeft(a, b);
         }
-        
     }
     
     public static void getDaysLeft(LocalDate one, LocalDate two) {
@@ -48,7 +50,9 @@ public class DueDateCalculator {
     
     //Overloaded Method to take strings (custom input)
     public static void getDaysLeft(String date1, String date2){
-        if(date1.charAt(2) == '/'){
+        
+        if(date1.length() == 10 && date1.charAt(2) == '/' && date1.charAt(5) == '/' 
+                && date2.charAt(2) == '/' && date2.charAt(5) == '/' && date2.length() == 10){
             DateTimeFormatter form = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 
             LocalDate one = LocalDate.parse(date1, form);
@@ -57,18 +61,28 @@ public class DueDateCalculator {
             getDaysLeft(one, two);
         
         }
-        else if(date1.charAt(2) == '-' ){
-            DateTimeFormatter form = DateTimeFormatter.ofPattern("MM-dd");
-
-            LocalDate one = LocalDate.parse(date1, form);
-            LocalDate two = LocalDate.parse(date2, form);
         
+        else if( date1.length() == 5 && date1.charAt(2) == '-'  && date2.charAt(2) == '-' && date2.length() == 5){
+            
+            String d1 = date1.concat("-2022");
+            String d2 = date2.concat("-2022");
+            
+            DateTimeFormatter form = DateTimeFormatter.ofPattern("MM-dd-yyyy");
+
+            LocalDate one = LocalDate.parse(d1, form);
+            LocalDate two = LocalDate.parse(d2, form);
+            
+            if(one.isBefore(java.time.LocalDate.now())) one = one.withYear(2023);
+            if(two.isBefore(java.time.LocalDate.now())) two = two.withYear(2023);            
+            
             getDaysLeft(one, two);
         }
         
         else {
             String e = "Invalid input format";
             System.err.println(e);
+            
+            getUserInput();
         }
         
     }
