ede3bea9e00614d6d28da79b9c3ca73eda359cf8
Evelyn Velez <67163723+evs09@users.noreply.github.com>
2022-10-23 20:01:33 -0400

    Allow user to select between saved dates in bash script

--- lab1/due_date_calculator.sh
+++ lab1/due_date_calculator.sh
@@ -1,21 +1,45 @@
 #!/bin/bash
 
-days_until_due(){
-    d1=$(date -j -f "%b %d %Y" "Sep 28 2022" +%s)
-    d2=$(date -j -f "%b %d %Y" "Oct 26 2022" +%s)
-    d3=$(date -j -f "%b %d %Y" "Nov 23 2022" +%s)
-    d4=$(date -j -f "%b %d %Y" "Dec 07 2022" +%s)
-    now=`date +%s`
+d1=$(date -j -f "%b %d %Y" "Sep 28 2022" +%s)
+d2=$(date -j -f "%b %d %Y" "Oct 26 2022" +%s)
+d3=$(date -j -f "%b %d %Y" "Nov 23 2022" +%s)
+d4=$(date -j -f "%b %d %Y" "Dec 07 2022" +%s)
+now=`date +%s`
 
-    date1=$(date -j -f "%b %d %Y" "Sep 28 2022" +%D)
-    date2=$(date -j -f "%b %d %Y" "Oct 26 2022" +%D)
-    date3=$(date -j -f "%b %d %Y" "Nov 23 2022" +%D)
-    date4=$(date -j -f "%b %d %Y" "Dec 07 2022" +%D)
+date1=$(date -j -f "%b %d %Y" "Sep 28 2022" +%D)
+date2=$(date -j -f "%b %d %Y" "Oct 26 2022" +%D)
+date3=$(date -j -f "%b %d %Y" "Nov 23 2022" +%D)
+date4=$(date -j -f "%b %d %Y" "Dec 07 2022" +%D)
 
-    printf "%d days left until $date1 \n" "$(( (d1-now)/86400 ))"
-    printf "%d days left until $date2 \n" "$(( (d2-now)/86400 ))"
-    printf "%d days left until $date3 \n" "$(( (d3-now)/86400 ))"
-    printf "%d days left until $date4 \n" "$(( (d4-now)/86400 ))"
+dateslist=($date1 $date2 $date3 $date4)
+calculatelist=($d1 $d2 $d3 $d4)
+
+days_left(){
+    d="$(( ($1 - now)/86400 ))"
+    echo $d
 }
 
-days_until_due
+inputHandler(){
+    if [ $1 -eq 1 ]
+    then
+        printf " $(days_left $d1) days left until $date1 \n"
+    elif [ $1 -eq 2 ]
+    then
+        printf " $(days_left $d2) days left until $date2 \n"
+    elif [ $1 -eq 3 ]
+    then
+        printf " $(days_left $d3) days left until $date3 \n"
+    else 
+        printf " $(days_left $d4) days left until $date4 \n"
+    fi 
+}
+
+echo "Select one of the following: "
+
+for (( i=1 ; i<5 ; i++ ))
+do
+    echo [ $i ] ${dateslist[$i-1]}
+done
+
+read input
+inputHandler $input
